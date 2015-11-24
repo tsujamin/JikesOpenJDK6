13510c4ae3156c0da906f3db8fca5ae024003860
Benjamin George Roberts <benjamin.roberts@anu.edu.au>
2015-11-24 12:13:04 +1100

    container demo now accepts jdk classpath as parameter

--- src/ContainerDemo.java
+++ src/ContainerDemo.java
@@ -1,14 +1,17 @@
-
-
 import org.jikesrvm.scheduler.MainThread;
 import org.jikesrvm.classloader.OpenJDKContainerClassLoader;
 
 public class ContainerDemo {
     public static void main(String[] args) {
-        for(int i = 0; i < 10; i++) {
-            OpenJDKContainerClassLoader cl = new OpenJDKContainerClassLoader();
-            MainThread main = new MainThread(args, cl);
-            main.start();
-        }
+    	if(args.length != 2) {
+    		System.out.println("ContainerDemo classpath class");
+    	} else {
+	        for(int i = 0; i < 10; i++) {
+	            new MainThread(
+	            		new String[] {args[1]}, 
+	            		new OpenJDKContainerClassLoader(args[0]))
+	            .start();
+	        }
+    	}
     }
 }
