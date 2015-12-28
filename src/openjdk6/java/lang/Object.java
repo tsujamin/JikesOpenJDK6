111b5680e94ec1d9dc9cc895dfbb2a02d94bac1e
Benjamin George Roberts <benjamin.roberts@anu.edu.au>
2015-12-29 00:06:21 +1100

    added Helloworld Example

--- src/openjdk6/java/lang/Object.java
+++ src/openjdk6/java/lang/Object.java
@@ -2,15 +2,24 @@ package openjdk6.java.lang;
 
 import org.vmmagic.pragma.ReplaceClass;
 import org.vmmagic.pragma.ReplaceMember;
+import org.jikesrvm.VM;
 import org.jikesrvm.objectmodel.ObjectModel;
+import org.jikesrvm.runtime.ArchEntrypoints;
 
 @ReplaceClass("Ljava.lang.Object;")
 public class Object {
+
 	@ReplaceMember
 	private static void registerNatives() {}
 
 	@ReplaceMember
 	public int hashCode() {
 		return ObjectModel.getObjectHashCode(this);
+	}
+	
+	@ReplaceMember("getClass")
+	public java.lang.Class<?> getClassReplacement() {
+		VM.sysWriteln("Called getClass");
+		return null;
 	}
 }
